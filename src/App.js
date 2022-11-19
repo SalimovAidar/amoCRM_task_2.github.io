e649f5a20ad288825476f526225cbc5ae3e37f9c
Aidar Salimov <aidar97@bk.ru>
2022-11-19 13:16:22 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './style/style.scss';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderItem.jsx
@@ -0,0 +1,9 @@
+export default ({ title }) => {
+  return (
+    <li className="header__item">
+      <a href="#" className="header__link">
+        {title}
+      </a>
+    </li>
+  )
+}
